fb9ff2a54e0a1c713593ba201f0f17bb45aae2fd
goonerDroid <williamj949@gmail.com>
2017-12-23 15:18:25 +0530

    *refractoring rem classes and XML files

--- app/src/main/java/com/sublime/dragplayer/utils/Preferences.java
+++ app/src/main/java/com/sublime/dragplayer/utils/Preferences.java
@@ -12,6 +12,18 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class Preferences {
 
+    private static final String MOVIE_URI = "movie_uri";
+    private static final String MOVIE_SEEK = "movie_seek";
+    private static final String PERMISSION_GRANTED = "permission_granted";
+    private static final String VIDEO_PREF = "video_pref";
+
+
+    private static final String KEY_URI = "key_uri";
+    private static final String KEY_SEEK_COUNT = "key_seek_count";
+    private static final String KEY_DRAW_PERMISSION = "key_draw_permissions";
+    private static final String KEY_VIDEO_STATE = "key_video_state";
+
+
     private Context context;
 
     public Preferences(Context context) {
@@ -20,51 +32,51 @@ public class Preferences {
     }
 
     public void saveMovieURI(String movieURI) {
-        SharedPreferences prefs = context.getSharedPreferences("MOVIE_URI", MODE_PRIVATE);
+        SharedPreferences prefs = context.getSharedPreferences(MOVIE_URI, MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("URI", movieURI);
+        editor.putString(KEY_URI, movieURI);
         editor.apply();
     }
 
     public void saveMovieSeekCount(int movieSeekCount) {
-        SharedPreferences prefs = context.getSharedPreferences("MOVIE_SEEK", MODE_PRIVATE);
+        SharedPreferences prefs = context.getSharedPreferences(MOVIE_SEEK, MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt("SEEK_COUNT", movieSeekCount);
+        editor.putInt(KEY_SEEK_COUNT, movieSeekCount);
         editor.apply();
     }
 
 
     public void isDrawPermissionGranted(boolean isPermissionGranted) {
-        SharedPreferences prefs = context.getSharedPreferences("PERMISSION_GRANTED", MODE_PRIVATE);
+        SharedPreferences prefs = context.getSharedPreferences(PERMISSION_GRANTED, MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean("DRAW_PERMISSION", isPermissionGranted);
+        editor.putBoolean(KEY_DRAW_PERMISSION, isPermissionGranted);
         editor.apply();
     }
 
     public void isVideoPlaying(boolean isVideoPlaying) {
-        SharedPreferences prefs = context.getSharedPreferences("VIDEO_PREF", MODE_PRIVATE);
+        SharedPreferences prefs = context.getSharedPreferences(VIDEO_PREF, MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean("VIDEO_STATE", isVideoPlaying);
+        editor.putBoolean(KEY_VIDEO_STATE, isVideoPlaying);
         editor.apply();
     }
 
 
 
     public String getMovieURI() {
-        return context.getSharedPreferences("MOVIE_URI", MODE_PRIVATE).getString("URI","");
+        return context.getSharedPreferences(MOVIE_URI, MODE_PRIVATE).getString(KEY_URI,"");
     }
 
     public int getMovieSeekCount() {
-        return context.getSharedPreferences("MOVIE_SEEK", MODE_PRIVATE).getInt("SEEK_COUNT",0);
+        return context.getSharedPreferences(MOVIE_SEEK, MODE_PRIVATE).getInt(KEY_SEEK_COUNT,0);
     }
 
     public boolean isDrawPermissionGranted() {
-        return context.getSharedPreferences("PERMISSION_GRANTED", MODE_PRIVATE).getBoolean("DRAW_PERMISSION",false);
+        return context.getSharedPreferences(PERMISSION_GRANTED, MODE_PRIVATE).getBoolean(KEY_DRAW_PERMISSION,false);
     }
 
 
     public boolean isVideoPlaying() {
-        return context.getSharedPreferences("VIDEO_PREF", MODE_PRIVATE).getBoolean("VIDEO_STATE",false);
+        return context.getSharedPreferences(VIDEO_PREF, MODE_PRIVATE).getBoolean(KEY_VIDEO_STATE,false);
     }
 
 
